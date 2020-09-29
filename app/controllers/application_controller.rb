a9cd10912dff3f232f65650d8905b88958240571
fuku1988 <momo.c.1988333@gmail.com>
2020-09-29 22:05:36 +0900

    Association/ ストロングパラメーターの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
   # before_action :basic_auth
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   def configure_permitted_parameters
-    devise_perameter_sanitizer.permit(:sing_up,key: [:name])
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date])
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :items
+  has_many :buys
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_day,presence: true
+
 end
