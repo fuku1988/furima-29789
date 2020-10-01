84921e113902038fc0ee248570c905ffb1a7b1b3
fuku1988 <momo.c.1988333@gmail.com>
2020-10-01 16:31:27 +0900

    コードレビュー後の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,17 @@ class User < ApplicationRecord
   has_many :items
   has_many :buys
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, {presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
-  VALID_PASSWORD_REGEX = /[a-z\d]{6,}/i
-  validates :password, {presence: true, format: { with: VALID_PASSWORD_REGEX }}
-  validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_date,presence: true
+  with_options presence: true do
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+    VALID_PASSWORD_REGEX = /[a-z\d]{6,}/i
+    validates :password, format: { with: VALID_PASSWORD_REGEX }
+    validates :nickname
+    validates :first_name
+    validates :last_name
+    validates :last_name_kana
+    validates :first_name_kana
+    validates :birth_date
+  end
 
 end
