4f62379925a0416d00d40222fc5a6ad5b5924fb6
fuku1988 <momo.c.1988333@gmail.com>
2020-10-04 16:29:07 +0900

    発送元の地域の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,15 +3,18 @@ class Item < ApplicationRecord
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
   belongs_to_active_hash :delivery_cost
+  belongs_to_active_hash :delivery_area
   
   validates :category
   validates :condition
   validates :delivery_cost
+  validates :delivery_area
 
   with options numericality: { other_then: 1 }
     validates :category_id
     validates :condition_id
-    validates :delivery_cost
+    validates :delivery_cost_id
+    validates :delivery_area_id
   end
    
   belongs_to :user
