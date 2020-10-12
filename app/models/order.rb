2d3ce6548cd1ae9e579010d9f239d3ac6002dd5f
fuku1988 <momo.c.1988333@gmail.com>
2020-10-12 12:44:37 +0900

    ordersコントローラーの作成

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,8 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one :address
+
+  validates :address, presence: true
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items
+  resources :items 
+  resources :orders
+  end
 end
