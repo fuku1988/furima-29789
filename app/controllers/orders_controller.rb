545e06f8ed2c71d5c334dd931e18378fdc699729
fuku1988 <momo.c.1988333@gmail.com>
2020-10-13 17:17:01 +0900

    決算機能のサーバーサイドの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order = Order.new(order_params)
     if @order.valid?
+      pay_item
       @order.save
       return redirect_to item_path
     else
@@ -21,6 +22,15 @@ class OrdersController < ApplicationController
   
   def order_params
     params.permit(:order).merge(token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
