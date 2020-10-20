36409e9f40d8ebda708419c39c47a2209de374af
fuku1988 <momo.c.1988333@gmail.com>
2020-10-21 00:41:48 +0900

    transactionsの単体テストコードを日本語に修正

--- app/models/transactions.rb
+++ app/models/transactions.rb
@@ -12,12 +12,12 @@ class Transactions
   end
 
 
-  with_options numericality: { other_than: 1, message: " --- 以外を洗濯してください" } do
+  with_options numericality: { other_than: 1, message: "は --- 以外を選択してください" } do
     validates :prefecture_id
   end
 
-  validates :postal_code,format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
-  validates :phone_number,format: {with:  /\A[0-9]{9,11}\z/ }
+  validates :postal_code,format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "は半角数字で入力してください（ハイフンが必要です）"}
+  validates :phone_number,format: {with:  /\A[0-9]{9,11}\z/ ,message: "は半角数字で入力してください（ハイフンは不要です）"}
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
